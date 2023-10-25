518ba6001600e24238c88df23f96b992e9bdf49e
asru-islam <arasru01@gmail.com>
2023-10-25 15:47:43 +0600

    practice nested loop

--- C-Programming/Semester_1/week-4/module-13/pattern_1.c
+++ C-Programming/Semester_1/week-4/module-13/pattern_1.c
@@ -2,6 +2,17 @@
 
 int main()
 {
+    int n , k=1;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= k; j++)
+        {
+            printf("*");
+        }
+        k++;
+        printf("\n");
+        }
     
     return 0;
 }
