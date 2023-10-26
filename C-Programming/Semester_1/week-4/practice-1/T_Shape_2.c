8fbcc709e9dd1015d94551c1f83b7b3cc4a4cba9
asru-islam <arasru01@gmail.com>
2023-10-27 04:01:36 +0600

    problem solve 2

---
+++ C-Programming/Semester_1/week-4/practice-1/T_Shape_2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int m = n - 1, k = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= k; j++)
+        {
+            printf("*");
+        }
+        k += 2;
+        m--;
+        printf("\n");
+    }
+
+    return 0;
+}
