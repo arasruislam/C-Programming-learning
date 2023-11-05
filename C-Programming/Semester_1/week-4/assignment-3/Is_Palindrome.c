4c07917e0dfcc6d55cd045d8220e1d60e5923c3f
asru-islam <arasru01@gmail.com>
2023-11-05 12:10:38 +0600

    complete the assignment 3

--- C-Programming/Semester_1/week-4/assignment-3/Is_Palindrome.c
+++ C-Programming/Semester_1/week-4/assignment-3/Is_Palindrome.c
@@ -1,11 +1,42 @@
+// #include <stdio.h>
+// #include <string.h>
+
+// int is_palindrome(char str[])
+// {
+//     for (int i = 0; i < strlen(str); i++)
+//     {
+//         printf("%s", str[i]);
+//     }
+
+// }
+// int main()
+// {
+//     char s[1001];
+//     scanf("%s", s);
+
+//     for (int i = 0; i <= strlen(s); i++)
+//     {
+//         printf("%s", s[i]);
+//     }
+//     // is_palindrome(s);
+//     return 0;
+// }
+
 #include <stdio.h>
-
-int is_palindrome(){
-
-}
+#include <string.h>
 
 int main()
 {
-    
+    char c[1001];
+    for (int i = 0; i < 1001; i++)
+    {
+        scanf("%c", &c[i]);
+    }
+
+    for (int i = 0; i < 1001; i++)
+    {
+        printf("%c\n", c[i]);
+    }
+
     return 0;
 }
