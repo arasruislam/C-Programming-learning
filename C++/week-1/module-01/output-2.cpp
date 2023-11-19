b127eeebabc0b25cb7743cde1b9c24ba6256bf1a
asru-islam <arasru01@gmail.com>
2023-11-19 22:59:02 +0600

    update file

---
+++ C++/week-1/module-01/output-2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main()
+{
+    int a = 10;
+    long long int b = 1000000000000;
+    float c = 1.4;
+    double d = 1.23232343;
+    char e = 'A';
+
+    // no need to give formate specifier in c++
+    //* std::cout<< b;
+
+    //* std::cout<<"My favorite number is " << a;
+
+    //* std::cout << "My " << a << " favorite number is";
+
+    // std::cout << "My " << a << " " << "favorite number is";
+
+    // std::cout << a << "\n"
+    //           << c;
+
+    std::cout << a << std::endl
+              << b << std::endl
+              << c << std::endl
+              << d << std::endl
+              << e;
+
+    return 0;
+}
