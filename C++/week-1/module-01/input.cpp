543e45e63465fa4f8c2a3ce16b9b92d197e158df
arasruislam <arasru01@gmail.com>
2023-11-22 10:09:32 +0600

    in c++, today learn eof and setprecision

--- C++/week-1/module-01/input.cpp
+++ C++/week-1/module-01/input.cpp
@@ -4,9 +4,14 @@ using namespace std;
 int main()
 {
 
-    int a;
-    cin >> a;
-    cout << a << endl;
+    // int a;
+    // cin >> a;
+    // cout << a << endl;
+
+    char c;
+    cin >> c;
+    // cout << c << endl;
+    cout << int(c) << endl; // ascii value == 65;
 
     return 0;
 }
