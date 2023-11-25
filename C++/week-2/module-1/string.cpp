631d78d34306c9f913b28d56af5c1944a7f5c79b
arasruislam <arasru01@gmail.com>
2023-11-25 13:39:58 +0600

    learning today string in c++

---
+++ C++/week-2/module-1/string.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    /*
+    string a = "Hello";
+    a = "Ami change hoye jabo";
+    cout << a << endl;
+    */
+
+    string a = "Hello";
+    string b = "Hello";
+
+    if (a == b)
+    {
+        cout << "Same" << endl;
+    }
+    else
+    {
+        cout << "Not Same" << endl;
+    }
+
+    return 0;
+}
