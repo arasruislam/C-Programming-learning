790621dd04680cf9fa87f8ec18b334bba3214031
arasruislam <arasru01@gmail.com>
2023-12-02 06:40:50 +0600

    learn data structure binary search and prefix sum

---
+++ Data-Structure/Module-3/prefix_sum_array.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    long long n, q;
+    cin >> n >> q;
+    long long a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    long long prefix[n];
+    prefix[0] = a[0];
+    for (int i = 1; i < n; i++)
+    {
+        prefix[i] = a[i] + prefix[i - 1];
+    }
+
+    while (q--)
+    {
+
+        long long l, r;
+        cin >> l >> r;
+        l--;
+        r--;
+
+        long long sum;
+
+        if (l == 0)
+        {
+            sum = prefix[r];
+        }
+        else
+        {
+            sum = prefix[r] - prefix[l - 1];
+        }
+        cout << sum << endl;
+    }
+
+    return 0;
+}
