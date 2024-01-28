35db3b81da5d3fb09682b684db15a1a27cff5573
arasruislam <arasru01@gmail.com>
2024-01-28 10:36:54 +0600

    learn count tree node

---
+++ Data-Structure/week-5/practice-1/level_order.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *left;
+    Node *right;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void levelOrder(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        Node *front = q.front();
+        q.pop();
+
+        cout << front->value << " ";
+
+        if (front->left)
+            q.push(front->left);
+        if (front->right)
+            q.push(front->right);
+    }
+}
+
+int main()
+{
+    // Create Nodes
+    Node *root = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *e = new Node(60);
+    Node *f = new Node(70);
+    Node *g = new Node(80);
+    Node *h = new Node(90);
+    Node *i = new Node(100);
+
+    // connection
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    a->right = h;
+    c->right = e;
+    b->right = d;
+    d->left = f;
+    d->right = g;
+    h->right = i;
+
+    // call
+    levelOrder(root);
+
+    return 0;
+}
