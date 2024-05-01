8d2e3276d9f399550af232d8a9b65a5b37760273
arasruislam <arasru01@gmail.com>
2024-05-01 11:19:00 +0600

    practice adjacency matrix.

--- Algorithms/week-1/module-2/bfs_traversal.cpp
+++ Algorithms/week-1/module-2/bfs_traversal.cpp
@@ -6,21 +6,11 @@ int main()
 {
     int n, e;
     cin >> n >> e;
-    vector<int> mat[n];
-
     while (e--)
     {
         int a, b;
         cin >> a >> b;
-
-        // connection
-        mat[a].push_back(b);
-        mat[b].push_back(a);
-    }
-
-    for (int x : mat[4])
-    {
-        cout << x << " ";
+        
     }
 
     return 0;
