f658bae821f1faecb1a432eb48c044248c84965f
arasruislam <arasru01@gmail.com>
2024-05-01 10:51:15 +0600

    practice adjacency matrix.

---
+++ Algorithms/week-1/module-1/adjancency_list.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+    }
+
+    return 0;
+}
