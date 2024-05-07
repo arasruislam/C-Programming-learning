b64bf5605281f684fc5a788444855a10162f7467
arasruislam <arasru01@gmail.com>
2024-05-07 19:19:13 +0600

    learn dfs algorithm

---
+++ Algorithms/week-1/module-3/2d_grid.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int a[20][20];
+bool vis[20][20];
+vector<pair<int, int>> d = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+void dfs(int si, int sj)
+{
+    cout << si << " " << sj << endl;
+    vis[si][sj] = true;
+
+    for (int i = 0; i < 4; i++)
+    {
+        int ci = si + d[i].first;
+        int cj = sj + d[i].second;
+        cout << ci << " " << cj << endl;
+    }
+}
+
+int main()
+{
+    int n, m;
+    cin >> n>>m;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+
+    int si, sj;
+    cin >> si >> sj;
+    memset(vis, false, sizeof(vis));
+    dfs(si, sj);
+
+    return 0;
+}
