d1601c3a57f2ed6eace57a3f97cca7132003c1e3
arasruislam <arasru01@gmail.com>
2024-07-02 11:49:15 +0600

    set models and admin panel

--- Python/Django/week-4/Blog/posts/models.py
+++ Python/Django/week-4/Blog/posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from categories.models import Category
+from author.models import Author
+
 
 # Create your models here.
+class Post(models.Model):
+    name = models.CharField(max_length=50)
+    content = models.TextField()
+
+    # relation with category => many to many relation
+    category = models.ManyToManyField(Category)
+
+    # relation with author => one to many | many to one
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
