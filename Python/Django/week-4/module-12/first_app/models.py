33ebcb8a007c9840762248f56488dc40526467a4
arasruislam <arasru01@gmail.com>
2024-07-02 07:25:19 +0600

    make migration

--- Python/Django/week-4/module-12/first_app/models.py
+++ Python/Django/week-4/module-12/first_app/models.py
@@ -6,3 +6,4 @@ class Student(models.Model):
     name = models.CharField(max_length=20)
     roll = models.IntegerField(primary_key=True)
     address = models.TextField()
+    father_name = models.TextField(default="rahim")
