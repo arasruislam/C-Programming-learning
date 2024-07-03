6bc6a14edd5a35d172ebd5f754ea53e23d582f4f
arasruislam <arasru01@gmail.com>
2024-07-03 18:06:41 +0600

    Tailwind done

--- Python/Django/week-4/demo/blog/settings.py
+++ Python/Django/week-4/demo/blog/settings.py
@@ -25,7 +25,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # app's
     "author"
+    # tailwind
+    "tailwind",
+    "theme",
 ]
 
 MIDDLEWARE = [
