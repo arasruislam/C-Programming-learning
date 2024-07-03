415afc7fd713acd8cfa9761093fcf4e0652a8eea
arasruislam <arasru01@gmail.com>
2024-07-03 09:50:57 +0600

    add done

--- Python/Django/week-4/asru/categories/urls.py
+++ Python/Django/week-4/asru/categories/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path("add/", views.addCategory, name="add_category"),
-    ]
+    path("add/", views.addCategory, name="add_category"),
+]
