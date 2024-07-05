154ecb244887c8fc7a74eae38516edec8d709e0e
arasruislam <arasru01@gmail.com>
2024-07-05 16:10:50 +0600

    ...

--- Python/Django/week-5/authentication_authorization/first_app/views.py
+++ Python/Django/week-5/authentication_authorization/first_app/views.py
@@ -5,11 +5,15 @@ from django.contrib import messages
 
 # Create your views here.
 def home(request):
+    return render(request, "home.html")
+
+
+def signup(request):
     if request.method == "POST":
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            messages.success(request, 'User created successfully')
+            messages.success(request, "User created successfully")
             form.save()
     else:
         form = RegistrationForm()
-    return render(request, "home.html", {"form": form})
+    return render(request, "signup.html", {"form": form})
