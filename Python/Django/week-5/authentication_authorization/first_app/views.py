2cac9001d35c22386e14663c6ed1921893056b31
arasruislam <arasru01@gmail.com>
2024-07-05 13:25:37 +0600

    Built-in messages django

--- Python/Django/week-5/authentication_authorization/first_app/views.py
+++ Python/Django/week-5/authentication_authorization/first_app/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from .forms import RegistrationForm
+from django.contrib import messages
 
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            messages.success(request, 'User created successfully')
+            form.save()
+    else:
+        form = RegistrationForm()
+    return render(request, "home.html", {"form": form})
--- Python/Django/week-5/authentication_authorization/project_eight/settings.py
+++ Python/Django/week-5/authentication_authorization/project_eight/settings.py
@@ -21,7 +21,13 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "first_app",
+    # crispy
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
+# crispy
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
