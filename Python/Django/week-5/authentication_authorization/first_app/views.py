8061c75ee7da6e0fc840f751b4f61bd94b86d406
arasruislam <arasru01@gmail.com>
2024-07-05 16:29:14 +0600

    login page authentication done

--- Python/Django/week-5/authentication_authorization/first_app/views.py
+++ Python/Django/week-5/authentication_authorization/first_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
@@ -17,3 +19,24 @@ def signup(request):
     else:
         form = RegistrationForm()
     return render(request, "signup.html", {"form": form})
+
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+
+            user = authenticate(username=name, password=password)
+
+            if user is not None:
+                login(request, user)
+                redirect("profile")
+    else:
+        form = AuthenticationForm()
+    return render(request, "login.html", {"form": form})
+
+
+def profile(request):
+    return render(request, "profile.html")
