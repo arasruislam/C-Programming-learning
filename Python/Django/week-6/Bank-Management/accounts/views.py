4e6f5dd5e874ab1b7e9d3ee3c4fb8796e963e6eb
arasruislam <arasru01@gmail.com>
2024-07-09 02:23:12 +0600

    part-1 done

--- Python/Django/week-6/Bank-Management/accounts/views.py
+++ Python/Django/week-6/Bank-Management/accounts/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.edit import FormView
 from django.contrib.auth import login, logout
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserUpdateForm
 from django.urls import reverse_lazy
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
 
 
 # Registration view
@@ -31,8 +32,29 @@ class UserLoginView(LoginView):
 
 
 # Logout View
-class UserLogoutView(LogoutView):
-    def get_success_url(self):
-        if self.request.user.is_authenticated:
-            logout(self.request)
-        return reverse_lazy("home")
+# class UserLogoutView(LogoutView):
+#     def get_success_url(self):
+#         if self.request.user.is_authenticated:
+#             logout(self.request)
+#         return reverse_lazy("home")
+
+
+def UserLogoutView(request):
+    logout(request)
+    return redirect("home")
+
+
+# User Profile Update View
+class UserBankAccountUpdateView(View):
+    template_name = "accounts/profile.html"
+
+    def get(self, request):
+        form = UserUpdateForm(instance=request.user)
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request):
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("profile")  # Redirect to the user's profile page
+        return render(request, self.template_name, {"form": form})
