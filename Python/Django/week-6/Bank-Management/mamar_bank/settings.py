0679e8b7d3a1d12679ee899ced8c076e2d0945c7
arasruislam <arasru01@gmail.com>
2024-07-09 11:03:55 +0600

    create models transaction

--- Python/Django/week-6/Bank-Management/mamar_bank/settings.py
+++ Python/Django/week-6/Bank-Management/mamar_bank/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     # App
     "accounts",
     "core",
+    "transactions",
 ]
 
 MIDDLEWARE = [
--- Python/Django/week-6/Own_Bank/mamar_bank/settings.py
+++ Python/Django/week-6/Own_Bank/mamar_bank/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     # App
     "accounts",
     "core",
+    "transactions",
 ]
 
 MIDDLEWARE = [
