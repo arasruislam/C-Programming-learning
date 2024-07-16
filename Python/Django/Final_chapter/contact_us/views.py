cf630cb790354354937a4854fd310a6f58726030
arasruislam <arasru01@gmail.com>
2024-07-16 18:09:27 +0600

    first api making done

--- Python/Django/Final_chapter/contact_us/views.py
+++ Python/Django/Final_chapter/contact_us/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ContactUsSerializer
+from .models import ContactUs
 
 # Create your views here.
+class ContactUsViewset(viewsets.ModelViewSet):
+    queryset = ContactUs.objects.all()
+    serializer_class = ContactUsSerializer
