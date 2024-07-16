ba057dcd715b164f4f86a2059c992c1d8b2ca6fa
arasruislam <arasru01@gmail.com>
2024-07-16 15:34:00 +0600

    Doctor model and review model done

--- Python/Django/Final_chapter/doctor/admin.py
+++ Python/Django/Final_chapter/doctor/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Doctor, AvailableTime, Specialization, Designation, Review
 
 # Register your models here.
+admin.site.register(AvailableTime)
+
+
+class SpecializationAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        "slug": ("name",),
+    }
+    list_display = ["name", "slug"]
+
+
+class DesignationAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        "slug": ("name",),
+    }
+    list_display = ["name", "slug"]
+
+admin.site.register(Specialization, SpecializationAdmin)
+admin.site.register(Designation, DesignationAdmin)
+admin.site.register(Doctor)
+admin.site.register(Review)
