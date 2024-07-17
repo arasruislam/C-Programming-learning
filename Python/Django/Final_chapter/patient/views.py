6075c46bae2d950d87cd320e3c728d49001fd7c1
arasruislam <arasru01@gmail.com>
2024-07-17 15:03:30 +0600

    Email confirmation done

--- Python/Django/Final_chapter/patient/views.py
+++ Python/Django/Final_chapter/patient/views.py
@@ -3,7 +3,17 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import viewsets
 from .models import Patient
-
+# Authenticated Related
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from rest_framework.authtoken.models import Token
+# for sending email
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.shortcuts import redirect
 
 # Create your views here.
 class PatientViewSet(viewsets.ModelViewSet):
@@ -19,5 +29,16 @@ class UserRegistrationView(APIView):
 
         if serializer.is_valid():
             user = serializer.save()
-            return Response("Form Submission Done")
+            token = default_token_generator.make_token(user)
+            uid = urlsafe_base64_encode(force_bytes(user.pk))
+            confirm_link = f"http://127.0.0.1:8000/patient/active/{uid}/{token}"
+            email_subject = "Confirm Your Email"
+            email_body = render_to_string(
+                "confirm_email.html", {"confirm_link": confirm_link}
+            )
+            email = EmailMultiAlternatives(email_subject, "", to=[user.email])
+            email.attach_alternative(email_body, "text/html")
+            email.send()
+
+            return Response("Check your mail for confirmation")
         return Response(serializer.errors)
