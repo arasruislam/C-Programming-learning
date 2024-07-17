d0c9050276dace6686591c0a57e92f26dec11c13
arasruislam <arasru01@gmail.com>
2024-07-17 08:19:47 +0600

    registration view done

--- Python/Django/Final_chapter/patient/urls.py
+++ Python/Django/Final_chapter/patient/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PatientViewSet
+from .views import PatientViewSet, UserRegistrationView
 
 router = DefaultRouter()
 router.register("list", PatientViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("register/", UserRegistrationView.as_view(), name="register"),
 ]
--- Python/Django/Final_chapter/patient/views.py
+++ Python/Django/Final_chapter/patient/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from .serializers import PatientSerializer, RegistrationSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework import viewsets
 from .models import Patient
-from .serializers import PatientSerializer
 
 
 # Create your views here.
 class PatientViewSet(viewsets.ModelViewSet):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
+
+# Registration View
+class UserRegistrationView(APIView):
+    serializer_class = RegistrationSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response("Form Submission Done")
+        return Response(serializer.errors)
