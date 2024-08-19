a146fd9273d829dced9a73b6e367bab2a840adc2
arasruislam <arasru01@gmail.com>
2024-08-19 17:05:08 +0600

    ..

--- Recap/recap_1/first_app/forms.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-
-
-class RegistrationForm(UserCreationForm):
-    class Meta:
-        model = User
-        # fields = "__all__"
-        fields = ["username", "first_name", "last_name", "email"]
