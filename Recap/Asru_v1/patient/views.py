4e6c6e3744b4af2ad513102f2817c9d974c4d267
arasruislam <arasru01@gmail.com>
2024-08-22 19:48:17 +0600

    patient api done

--- Recap/Asru_v1/patient/views.py
+++ Recap/Asru_v1/patient/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Patient
+from .serializers import PatientSerializer
+
 
 # Create your views here.
+class PatientUsViewSet(viewsets.ModelViewSet):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
