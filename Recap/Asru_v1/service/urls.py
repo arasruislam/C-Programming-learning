33d7ea850c6521c80cdb2030f9de2d2d42882ce7
arasruislam <arasru01@gmail.com>
2024-08-22 19:22:27 +0600

    service api done

--- Recap/Asru_v1/service/urls.py
+++ Recap/Asru_v1/service/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register(r"", views.ContactUsViewSet)
+router.register(r"", views.ServiceViewSet)
 
 
 urlpatterns = [
