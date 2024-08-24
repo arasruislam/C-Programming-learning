065e8f7920631f6aa166f6a2c8e3da7a66ad48ca
arasruislam <arasru01@gmail.com>
2024-08-24 09:29:04 +0600

    mail confirmation done

--- Recap/Asru_v2/patient/urls.py
+++ Recap/Asru_v2/patient/urls.py
@@ -9,6 +9,5 @@ router.register(r"list", views.PatientUsViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path("register/", views.UserRegistrationApiView.as_view(), name="register"),
+    path("active/<uid64>/<token>/", views.Activate, name="active"),
 ]
-
-
--- Recap/Asru_v2/patient/views.py
+++ Recap/Asru_v2/patient/views.py
@@ -10,7 +10,9 @@ from .models import Patient
 # for email sending
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+from django.contrib.auth.models import User
 from django.shortcuts import redirect
+
 
 # Create your views here.
 class PatientUsViewSet(viewsets.ModelViewSet):
@@ -31,11 +33,28 @@ class UserRegistrationApiView(APIView):
             print("uid: ", uid)
             confirm_link = f"http://127.0.0.1:8000/patient/active/{uid}/{token}"
             email_subject = "Confirm your email"
-            email_body = render_to_string("confirm_email.html", {"confirm link": confirm_link})
-            
+            email_body = render_to_string(
+                "confirm_email.html", {"confirm_link": confirm_link}
+            )
+
             email = EmailMultiAlternatives(email_subject, "", to=[user.email])
             email.attach_alternative(email_body, "text/html")
             email.send()
             return Response("Check your mail for confirmation")
 
         return Response(serializer.errors)
+
+
+def Activate(request, uid64, token):
+    try:
+        uid = urlsafe_base64_decode(uid64).decode()
+        user = User._default_manager.get(pk=uid)
+    except User.DoesNotExist:
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        return redirect("register")
+    else:
+        return redirect("register")
